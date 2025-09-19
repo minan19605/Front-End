e27036d49ef050014bdc99040fd0adfc7a66d58f
minan19605 <minan.19605@gmail.com>
2025-09-19 17:32:24 -0500

    change import file name

--- React/my-movie/src/pages/SearchedMovies.jsx
+++ React/my-movie/src/pages/SearchedMovies.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import { apiKey } from './MovieDetails';
+import { apiKey } from './share.jsx';
 
 import { useParams } from 'react-router-dom';
 import { Movie, MovieSkeleton} from './Recommended.jsx'
